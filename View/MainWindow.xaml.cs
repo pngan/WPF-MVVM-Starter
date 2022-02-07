9dac0b84a6df609679aa6b5176c7e934518905f3
Phillip Ngan <phillip.ngan@gmail.com>
2022-02-08 09:57:56 +1300

    Initial Starter app for WPF illustrating MVVM

---
+++ View/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+using ViewModel;
+
+namespace View
+{
+    public partial class MainWindow : Window
+    {
+        private readonly IMainViewModel m_viewModel;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public MainWindow(IMainViewModel viewModel) : this()
+        {
+            m_viewModel = viewModel;
+            m_viewModel.Start();
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            ((FrameworkElement)sender).DataContext = m_viewModel;
+        }
+    }
+}
--- WPF-MVVM-Starter/App.xaml.cs
+++ WPF-MVVM-Starter/App.xaml.cs
@@ -1,17 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+
+using Autofac;
+using Model;
+using ViewModel;
+using View;
 
 namespace WPF_MVVM_Starter
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            var builder = new ContainerBuilder();
+            RegisterDependencies(builder);
+            var container = builder.Build();
+
+            StartApplication(container);
+        }
+
+        private void RegisterDependencies(ContainerBuilder builder)
+        {
+            builder.RegisterType<MainModel>().As<IMainModel>();
+            builder.RegisterType<MainViewModel>().As<IMainViewModel>();
+            builder.RegisterType<MainWindow>();
+        }
+
+        private static void StartApplication(IContainer container)
+        {
+            var window = container.Resolve<MainWindow>();
+            window.Show();
+        }
     }
 }
